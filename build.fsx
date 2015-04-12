8a43ff68f97ab2b90675b74df8492f117c146b84
Tobias Kesselring <tobias.kesselring@incubeadvisory.com>
2015-04-12 13:53:22 +0200

    add build script

---
+++ build.fsx
@@ -0,0 +1,32 @@
+﻿#r @"packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+
+let resultsDir = "release"
+
+Target "Clean" (fun _ ->
+    DeleteDirs [resultsDir]
+)
+
+Target "Build" (fun _ ->
+    !! "/**/*.*proj"
+    |> MSBuildRelease resultsDir "Build"
+    |> Log "Build-Output: "
+)
+
+// no unit tests do exist for the moment.
+//Target "Tests" (fun _ ->
+//    !! "/**/Test.*.dll"
+//    ++ "/**/Test.*.exe"
+//    |> SetBaseDir resultsDir
+//    |> NUnitParallel (fun defaults -> { defaults with Framework = "net-4.5"})
+//)
+
+Target "Default" DoNothing
+
+"Clean"
+    ==> "Build"
+//    =?> ("Tests", not <| hasBuildParam "NoTests")
+    ==> "Default"
+
+RunTargetOrDefault "Default"
